98fae5b5e14ce0d5996c364903fd1570960aff32
user <user@user.com>
2025-06-25 15:26:46 +0100

    add textnode.py

---
+++ src/textnode.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = 'text'
+    BOLD_TEXT = 'bold'
+    ITALIC_TEXT = 'italic'
+    CODE_TEXT = 'code'
+    LINK = 'link'
+    IMAGE = 'image'
+
+
+class TextNode:
+    pass
+
