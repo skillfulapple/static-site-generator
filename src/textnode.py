6ea74b03d21e404b10619281c21072130cb643d0
user <user@user.com>
2025-06-25 15:43:05 +0100

    update textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -11,5 +11,17 @@ class TextType(Enum):
 
 
 class TextNode:
-    pass
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
+    def __eq__(self, node):
+        return (
+            self.text == node.text
+            and self.text_type == node.text_type
+            and self.url == node.url
+        )
+
+    def __repr__(self):
+        return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
