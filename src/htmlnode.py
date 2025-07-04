bf036e65d7639d9226a88051adac69bd25bb0172
user <user@user.com>
2025-07-04 12:23:15 +0100

    add class LeafNode to htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-# ...
-
 
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
@@ -18,3 +16,15 @@ class HTMLNode:
 
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag + self.props_to_html()}>{self.value}</{self.tag}>'
